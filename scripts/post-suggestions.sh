ea189f4e70de6fd4aa392185e7c6a08b22f35358
Dumplingpuff <jason.liang2012@gmail.com>
2016-03-01 21:57:18 -0500

    Test post suggestions with curl.

--- scripts/post-suggestions.sh
+++ scripts/post-suggestions.sh
@@ -1,6 +1,6 @@
 curl --include --request POST http://localhost:3000/suggestions \
   --header "Content-Type: application/json" \
-  --header "Authorization: Token token=10c81ed89f6c8a1be810a9b47f65f430" \
+  --header "Authorization: Token token=f998644f1045d298b271658e00a434d5" \
   --data '{
     "suggestion": {
       "name": "Jim",
